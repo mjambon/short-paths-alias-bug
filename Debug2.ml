838a2ec22b3ab1a4432f222ddcd331fb8bf18428
Martin Jambon <martin@mjambon.com>
2021-05-28 00:30:38 -0700

    Fix comment

--- Debug2.ml
+++ Debug2.ml
@@ -1,4 +1,4 @@
 open Debug1
 
-(* error message reports type 't_e' instead of 't_c' *)
+(* error message reports type 't_ee' instead of 't_c' *)
 let y = x + 1
--- Debug3.ml
+++ Debug3.ml
@@ -1,2 +1,2 @@
-(* error message reports type 'Debug1.t_e' instead of 'Debug1.t_c' *)
+(* error message reports type 'Debug1.t_ee' instead of 'Debug1.t_c' *)
 let y = Debug1.x + 1
